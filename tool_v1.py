9cf645c0c3605a2db9e2c818f54708be5944a7d4
kelkind <katherine.elkind@gmail.com>
2017-03-23 00:20:39 -0400

    Update tool_v1.py
    Implement second round of optimization for Cello modifications to original circuit.

--- tool_v1.py
+++ tool_v1.py
@@ -3,148 +3,106 @@
 
 from CelloProvider import CelloConnection
 from UCFEditor import UCFEditor
-from ToolOperations import *
 from ParseResults import *
-import numpy as np
+from ToolOperations import *
+from ToolFunctions import *
+#import numpy as np
 
   
 def get_user_inputs():
-    fname = input('UCF file name: ') # gets user input
-    gtype = input('Verilog File for Gate Type: ') # gets gate type (and, etc.)
-    if ".v" not in gtype:
-        gtype += ".v"
-    if "/" not in gtype:
-        gtype = "./" + gtype
+    UCFname = input('UCF file name: ') # gets user input
+    vFile = input('Verilog File: ') # gets gate type (AND, etc.)
+    if ".v" not in vFile:
+        vFile += ".v"
+    if "/" not in vFile:
+        vFile = "./" + vFile
     uname = input('Cello Username: ') # gets username
     pword = input('Cello Password: ') # gets password
-    jid = input('Define Job ID: ') # gets new job ID
-    rname = input('Results File Name: ') # gets filename to store results
-    if "." not in rname:
-        rname += ".txt" 
-    return fname, gtype, uname, pword, jid, rname
+    jobID = input('Define Job ID: ') # gets new job ID
+    resultsFile = input('Results File Name: ') # gets filename to store results
+    if "." not in resultsFile:
+        resultsFile += ".txt" 
+    return UCFname, vFile, uname, pword, jobID, resultsFile
 
-def run_Cello(fname, gtype, uname, pword, jid, rname):
+def run_Cello(UCFname, vFile, uname, pword, jobID, resultsFile):
+    """
+    Uploads the current UCF file, runs Cello, and downloads the logic circuit
+    Input: UCF file name, Circuit Type, Username, Password, Job ID, and
+        results file name.
+    """
     cello = CelloConnection((uname, pword))
     
-    cello.upload_ucf("./" + fname)
+    cello.delete_ucf(UCFname)
+    cello.upload_ucf("./" + UCFname)
 
-    cello.submit_job(jid, gtype, "./Inputs.txt","./Outputs.txt",
-                     "options=-UCF testUCF.json -plasmid false -eugene false")
+    ops = "-UCF " + UCFname + " -plasmid false -eugene false"
+    cello.submit_job(jobID, vFile, "./Inputs.txt","./Outputs.txt", ops)
 
-    for result in cello.get_job_results(jid):
+    for result in cello.get_job_results(jobID):
         if "logic_circuit.txt" in result.get_name():
-            result.download_to_file("./" + rname)
+            result.download_to_file("./" + resultsFile)
 
-def populate_lists(circuit_tree):
-    ed.load_ucf_from_json(fname)
-    reps, proms = [], [] 
-
-    for gate in circuit_tree:
-        if str(gate) in "Input":
-            proms += [gate.get_name()]
-        elif str(gate) in ("Nor", "Not"):
-            reps += [gate.get_name()]
-        
-    reps_params = {}
-    for gate in reps:
-        reps_params[gate] = ed.get_repressor_params(gate)
-    rp_copy = reps_params.copy()
-    
-    proms_params = {}
-    for p in proms:
-        with open("./Inputs.txt",'r') as inParams:
-            for line in inParams:
-                if p in line:
-                    parts = line.split()
-                    proms_params[p] = [parts[1], parts[2]]
-                    
-    assembly_order = []
-    for gate in circuit_tree:
-        if isinstance(gate, Output):
-            gate.populate_subtree_list(assembly_order)
-            
-    return assembly_order, proms, reps, proms_params, reps_params, rp_copy
 
 if __name__ == '__main__':
     ed = UCFEditor()
     
-    fname, gtype = "testUCF.json", "./0xFE.v"
-    uname, pword = "kelkind", "4L83rtO1"
-    jid = "KatTest"
-    rname = "Results_0xFE_BASE.txt"
+    UCFname = "testUCF.UCF.json"
+    vFile = "./0xFE.v"
+    uname = "kelkind"
+    pword = "4L83rtO1"
+    jobID = "Kat_Evan_Test"
+    resultsFile = "Results_0xFE.txt"
     
-#    run_Cello(fname, gtype, uname, pword, jid, rname)
+#    run_Cello(UCFname, vFile, uname, pword, jobID, resultsFile)
 
-    circuit_tree = parse_cello_results_file(rname)
+    circuit_tree = parse_cello_results_file(resultsFile)
 
-    assembly_order, proms, reps, proms_params, reps_params, rp_copy = populate_lists(circuit_tree)
+    ed.load_ucf_from_json(UCFname)
     
+    assembly_order, proms, proms_params, reps, reps_params, rp_copy = \
+        populate_params(circuit_tree, ed)
+
     score = scoreFunction(assembly_order, [proms_params,reps_params])
     print(score)
-    
-    # preallocates and defines values for the optimization loop
-    opsList = ['stretch','incSlope','decSlope','strProm','wkProm','strRBS',
-               'wkRBS']
-    temp_params, temp_score = {}, {}
-    oldScore, maxScoreVal = [score], 0 # initialized as the score to beat
 
-    # optimization loop
-    for g in assembly_order: 
-        if isinstance(g, Output):
-            pass
-        elif isinstance(g, Input):
-            pass
-        else:
-            i = 0
-            gate = g.get_name()
-            print('gate:',gate)
-            temp_params[gate], temp_score[gate] = {}, {}
-            while maxScoreVal <= oldScore[i-1]:
-                for op in opsList:
-                    if "stretch" in op:
-                        linmax = 1.5
-                    elif "Slope" in op:
-                        linmax = 1.05
-                    else:
-                        linmax = 2
-                    for sf in np.linspace(0.05,linmax,30):
-                        temp_params[gate][op] = modifyGate(reps_params[gate],op,sf)
-                        rp_copy[gate] = temp_params[gate][op]
-                        temp_score[gate][op] = scoreFunction(assembly_order,
-                                  [proms_params,rp_copy])      
-                maxScore = max(temp_score[gate], key=lambda key: temp_score[gate][key]) 
-                maxScoreVal = temp_score[gate][maxScore]
-                if i == 0:
-                    break
-                if maxScoreVal < oldScore[i-1]:
-                    rp_copy[reps[i-1]] = temp_params[reps[i-1]][oldScore]
-#            print(temp_score[gate])
-#            print(maxScore,':',temp_score[gate][maxScore])
-            rp_copy[gate] = temp_params[gate][maxScore] # keeps highest scoring change
-            try:
-                oldScore[i] = temp_score[gate][maxScore]
-            except:
-                oldScore += [temp_score[gate][maxScore]]
-            i += 1
- 
-    score = scoreFunction(assembly_order, [proms_params,reps_params])
-    print('Final Score:', score)
+    # optimizes the solution
+    change_log = {}
+    rp_copy, change_log = optimize(assembly_order, score, proms_params,
+                                   reps_params, rp_copy, change_log)
+
+    # calculates and prints the score of the optimized function
+    opt_score = scoreFunction(assembly_order, [proms_params,rp_copy])
+    print('Final Score:', opt_score)
     
     for gate in reps:    
 #        print(gate,":")
 #        print(rp_copy[gate])
         ed.set_repressor_params(gate,rp_copy[gate])
-        
-    fname = "mod" + fname    
-    ed.save_ucf_to_json("./" + fname)
     
-    rname = "verifyresults" + gtype[:-2] + ".txt"
-    run_Cello(fname, gtype, uname, pword, jid, rname)
+    ed.save_ucf_to_json("./" + UCFname)
     
-    circuit_tree_new = parse_cello_results_file(rname)
+    resultsFile = "verifyresults" + vFile[2:-2] + ".txt"
+    jobID = jobID + "1"
+#    run_Cello(UCFname, vFile, uname, pword, jobID, resultsFile)
+    
+    circuit_tree_new = parse_cello_results_file(resultsFile)
 
-    assembly_order_new, reps_params_new, rp_copy_new, proms_params_new = populate_lists(circuit_tree)
-    
-    new_score = scoreFunction(assembly_order_new, [proms_params_new,reps_params_new])
+    assembly_order_n, proms_n, proms_params_n, reps_n, reps_params_n, \
+        rp_copy_n = populate_params(circuit_tree_new, ed)
+
+    new_score = scoreFunction(assembly_order_n, [proms_params_n, reps_params_n])
     print(new_score)
-    
+#    print(change_log)
+    if new_score > opt_score:
+        opsList = ['stretch','incSlope','decSlope','strProm','wkProm','strRBS',
+               'wkRBS']
+        old = []
+        for key in change_log.keys():
+            if key not in reps_n:
+                old += [key]
+        for r in old:
+            del change_log[r]
+        rp_copy_n, change_log_n = optimize(assembly_order_n, opt_score,
+                                           proms_params_n, reps_params_n,
+                                           rp_copy_n, change_log)
+        print(scoreFunction(assembly_order_n, [proms_params,rp_copy_n]))
